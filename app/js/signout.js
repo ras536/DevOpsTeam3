e4389fe5528db9e3fc9225dd9fb409e101a6a3cc
Ryan <rasweeney94@yahoo.com>
2017-07-31 10:54:36 -0500

    Added signout functionality to front end

--- app/js/signout.js
+++ app/js/signout.js
@@ -1,10 +1,25 @@
   function signout() {
 
-    //****WRITE CODE TO ERASE ALL SIGNS OF BEING LOGGED IN*****/
+    jQuery.ajax({
+      type: "GET",
+      url: 'php/weatherapp.php',
+      data: {logout: true},
+      success: function (data) {
+        if(data != "0"){
+          console.log(data);
+            
+          document.getElementById("displayUsername").style.display="none";
+          document.getElementById("displayUsername").innerHTML = "<h2></h2>";
+          document.getElementById("signInStuff").style.display="inline-block";
+          document.getElementById("signOutBtn").style.display="none";
+          document.getElementById("locationList").style.display="none";
+          document.getElementById("addLocation").style.display="none";
 
-    document.getElementById("displayUsername").style.display="none";
-    document.getElementById("signInStuff").style.display="inline-block";
-    document.getElementById("signOutBtn").style.display="none";
-    document.getElementById("locationList").style.display="none";
-    document.getElementById("addLocation").style.display="none";
+        }else{
+          alert("Logout failure. Please try again.");
+        };
+      }
+    });
+
+
   }
